a20480daec8665e64623cb5cbc905d06c3953ba1
Iryna Yanushkevich <ireneyanushkevich@gmail.com>
2023-01-25 20:46:15 +0300

    2nd check

--- src/main/java/com/irinayanushkevich/restapi/model/Event.java
+++ src/main/java/com/irinayanushkevich/restapi/model/Event.java
@@ -17,8 +17,7 @@ public class Event {
     @JoinColumn(name = "file_id")
     private File file;
 
-    public Event() {
-    }
+    public Event() {    }
 
     public Event(Integer id, User user, File file) {
         this.id = id;
